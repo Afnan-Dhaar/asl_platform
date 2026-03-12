e415f525be8d6e758feb2301b96454a4348b34f2
Afnan-Dhaar <afnandhar786@gmail.com>
2026-03-12 15:04:32 +0530

    mail setup

--- contact.php
+++ contact.php
@@ -1,5 +1,13 @@
 <?php include("includes/header.php"); ?>
+<?php
+if(isset($_GET['success'])){
+echo "<p style='color:green'>Message sent successfully.</p>";
+}
 
+if(isset($_GET['error'])){
+echo "<p style='color:red'>Failed to send message.</p>";
+}
+?>
 <section class="page-hero">
 <h1>Contact Us</h1>
 <p>We would love to hear from you</p>
@@ -22,13 +30,13 @@
 
 <div class="contact-form">
 
-<form>
+<form method="POST" action="send_contact.php">
 
-<input type="text" placeholder="Your Name" required>
+<input type="text" name="name" placeholder="Your Name" required>
 
-<input type="email" placeholder="Email Address" required>
+<input type="email" name="email" placeholder="Email Address" required>
 
-<textarea placeholder="Your Message" rows="5"></textarea>
+<textarea name="message" placeholder="Your Message" rows="5" required></textarea>
 
 <button class="btn-primary">Send Message</button>
 
---
+++ send_contact.php
@@ -0,0 +1,56 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+$name = htmlspecialchars($_POST['name']);
+$email = htmlspecialchars($_POST['email']);
+$message = htmlspecialchars($_POST['message']);
+
+$mail = new PHPMailer(true);
+
+try{
+
+$mail->isSMTP();
+
+$mail->Host = 'smtp.gmail.com';
+$mail->SMTPAuth = true;
+
+$mail->Username = 'afnandhar786@gmail.com';
+$mail->Password = 'zdfs mosm tash pylf';
+
+$mail->SMTPSecure = 'tls';
+$mail->Port = 587;
+
+$mail->setFrom($email, $name);
+
+$mail->addAddress('afnandhar786@gmail.com');
+
+$mail->Subject = "New Contact Message";
+
+$mail->Body = "
+Name: $name
+
+Email: $email
+
+Message:
+$message
+";
+
+$mail->send();
+
+header("Location: contact.php?success=1");
+
+}catch(Exception $e){
+
+header("Location: contact.php?error=1");
+
+}
+
+}
